9de5e7d7991aa4cb91b28ca28fac248f31c51de7
MikuOgiwara <108834620+MikuOgiwara@users.noreply.github.com>
2026-06-15 21:18:50 +0200

    Fix build error

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   site: 'https://YumeYuriFr.github.io/YumeYuri/',
-  
+
   output: 'static',
   base: isDev ? '/' : '/YumeYuri/',
   integrations: [
