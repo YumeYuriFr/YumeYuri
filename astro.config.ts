0d87f4f17a3822eb56bcea4493ca90905cee0808
MikuOgiwara <108834620+MikuOgiwara@users.noreply.github.com>
2026-06-15 20:59:12 +0200

    update view off list of yuri and fix local dev

--- astro.config.ts
+++ astro.config.ts
@@ -16,16 +16,16 @@ import astrowind from './vendor/integration';
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
+const isDev = process.env.NODE_ENV === 'development';
 const hasExternalScripts = false;
 const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroIntegration)[] = []) =>
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
   site: 'https://YumeYuriFr.github.io/YumeYuri/',
-  base: '/YumeYuri/',
+  
   output: 'static',
-
+  base: isDev ? '/' : '/YumeYuri/',
   integrations: [
     tailwind({
       applyBaseStyles: false,
